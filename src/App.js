bf435754e1708cfd84e2ae1dce124c998f91f03e
ted <ted@warpedpuppy.com>
2020-09-02 18:46:54 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const style = {
+    width: '100vw', 
+    height: '100%',
+    maxWidth: '600px',
+    minHeight: '1072px',
+    padding: '0', 
+    margin:'0',
+    overflow: 'hidden',
+    border:'none',
+    display: 'block', 
+    margin: '0 auto',
+    outline: 'none',
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <iframe 
+        src="https://infallible-kare-f8cd09.netlify.app/" 
+        style={style}
+        title="Crystal Ball Fundraiser" ></iframe>
     </div>
   );
 }
